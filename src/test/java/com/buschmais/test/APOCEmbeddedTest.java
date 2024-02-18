7fd2623e0b4de37a86f1b3c7a862385071e39804
Dirk Mahler <dirk.mahler@buschmais.com>
2024-02-18 17:44:43 +0100

    minor cleanup

--- src/test/java/com/buschmais/test/APOCEmbeddedTest.java
+++ src/test/java/com/buschmais/test/APOCEmbeddedTest.java
@@ -1,7 +1,9 @@
 package com.buschmais.test;
 
-import java.io.File;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.neo4j.configuration.GraphDatabaseSettings;
@@ -12,18 +14,17 @@ import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.logging.log4j.Log4jLogProvider;
 
-import static java.util.Collections.singletonList;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 
 class APOCEmbeddedTest {
 
     @Test
     void apoc() {
-        File pluginDir = new File("target/neo4j/plugins");
-        DatabaseManagementService managementService = new DatabaseManagementServiceBuilder(new File("target/neo4j").toPath()).setConfig(
-                        GraphDatabaseSettings.plugin_dir, pluginDir.getAbsoluteFile()
-                                .toPath())
-                .setConfig(GraphDatabaseSettings.procedure_unrestricted, singletonList("apoc.*"))
+        Path neo4jDir = Paths.get("target/neo4j")
+                .toAbsolutePath();
+        Path pluginDir = neo4jDir.resolve("plugins");
+        DatabaseManagementService managementService = new DatabaseManagementServiceBuilder(neo4jDir).setConfig(GraphDatabaseSettings.plugin_dir, pluginDir)
+                .setConfig(GraphDatabaseSettings.procedure_unrestricted, List.of("apoc.*"))
                 .setUserLogProvider(new Log4jLogProvider(System.out))
                 .build();
         GraphDatabaseService graphDb = managementService.database(DEFAULT_DATABASE_NAME);
